76cfdefb7cd58619ea33f676fcd65c7b4ef1edb8
ibrahim04-al <55285989+ibrahim04-al@users.noreply.github.com>
2019-10-18 00:17:26 +0800

    Create alias modul.py

---
+++ alias modul.py
@@ -0,0 +1,15 @@
+import baim as alling
+def main():
+    p = 12
+    l = 6
+
+    luas = alling.luasPersegi(p, l)
+
+    print("PERSEGI")
+    print("Panjang\t: ", p)
+    print("Lebar\t: ", l)
+    print("Luas\t= ", luas)
+
+if __name__ == "__main__":
+    main()
+    
