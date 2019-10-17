c9435abc1a7cf274913437861d4014727a283fe0
ibrahim04-al <55285989+ibrahim04-al@users.noreply.github.com>
2019-10-18 00:18:06 +0800

    Create paket.py

---
+++ paket.py
@@ -0,0 +1,16 @@
+import tugas.baim
+
+def main():
+     p = 12
+     l = 6
+
+     luas = tugas.baim.luasPersegi(l, p)
+
+     print("PERSEGI")
+     print("Panjang\t: ", p)
+     print("Lebar\t: ", l)
+     print("Luas\t= ", luas)
+
+if __name__ == "__main__":
+    main()
+    
